b93f0845b38b6f3be5cb48974d3f7e8201fffc38
Isaac Doré <swessmusic@gmail.com>
2020-02-01 14:51:51 -0500

    With string values

--- src/HttpLib/HttpRequestMethod.java
+++ src/HttpLib/HttpRequestMethod.java
@@ -3,5 +3,16 @@ package HttpLib;
 public enum HttpRequestMethod {
 
     // Supported for assignment
-    POST, GET,
+    POST("POST"), GET("GET");
+
+    private String _val;
+
+    HttpRequestMethod(String _val) {
+        this._val = _val;
+    }
+
+    @Override
+    public String toString() {
+        return _val;
+    }
 }
