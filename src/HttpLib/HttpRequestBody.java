01a8137574e3e41cb9f86a0aeafce006bd8a063a
Noah <foolzo13@gmail.com>
2020-02-01 12:33:26 -0500

    HttpRequestBody constructor, toString(), getLength()

--- src/HttpLib/HttpRequestBody.java
+++ src/HttpLib/HttpRequestBody.java
@@ -3,7 +3,21 @@ package HttpLib;
 
 // TODO: Should receive string,
 public class HttpRequestBody {
-    int length = 0;
 
+    int length = 0;
+    String body;
+
+    public HttpRequestBody(String body) {
+        this.body = body;
+        this.length = body.length();
+    }
+
+    public String toString() {
+        return body;
+    }
+
+    public int getLength() {
+        return length;
+    }
 
 }
