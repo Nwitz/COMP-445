ed3f37ccc35ccfa45b106066fae9a6b5d6fae6b1
Isaac Doré <swessmusic@gmail.com>
2020-02-02 15:16:42 -0500

    Reverse enum lookup

--- src/HttpLib/HttpStatusCode.java
+++ src/HttpLib/HttpStatusCode.java
@@ -1,9 +1,19 @@
 package HttpLib;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+
 public enum HttpStatusCode {
     OK(200), Created(201), Accepted(202), NoContent(204),
     BadRequest(400), Unauthorized(401), Forbidden(403), NotFound(404),
     InternalServerError(500);
+
+    private static final HashMap<Integer, HttpStatusCode> lookup = new HashMap<Integer, HttpStatusCode>();
+
+    static {
+        for (HttpStatusCode s : EnumSet.allOf(HttpStatusCode.class))
+            lookup.put(s.getValue(), s);
+    }
 
     private int _val;
 
@@ -13,5 +23,9 @@ public enum HttpStatusCode {
 
     public int getValue() {
         return _val;
+    }
+
+    public static HttpStatusCode get(int code) {
+        return lookup.get(code);
     }
 }
