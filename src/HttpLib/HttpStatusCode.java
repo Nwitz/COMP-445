d3be73a2c8567475e535ad20545e999558611f40
Isaac Doré <swessmusic@gmail.com>
2020-02-09 20:11:49 -0500

    Entire redirections handled

--- src/HttpLib/HttpStatusCode.java
+++ src/HttpLib/HttpStatusCode.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 public enum HttpStatusCode {
     OK(200), Created(201), Accepted(202), NoContent(204),
+    MovedPermanently(301), MovedTemporarily(302), NotModified(304),
     BadRequest(400), Unauthorized(401), Forbidden(403), NotFound(404),
     InternalServerError(500);
 
