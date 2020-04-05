38174ebcea50fff7aa801cacfee207e53309c393
Isaac Doré <swessmusic@gmail.com>
2020-04-04 21:08:25 -0400

    Clean send PseudoTCP for now to build with more robust objects

--- src/HttpLib/protocol/UDP/PseudoTCP.java
+++ src/HttpLib/protocol/UDP/PseudoTCP.java
@@ -1,14 +1,13 @@
 package HttpLib.protocol.UDP;
 
+import HttpLib.ByteArrayUtils;
 import HttpLib.HttpRequest;
 import HttpLib.IRequestCallback;
 import HttpLib.protocol.IProtocol;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.concurrent.*;
 
 public class PseudoTCP implements IProtocol {
 
@@ -18,14 +17,28 @@ public class PseudoTCP implements IProtocol {
 
         // Open Socket
         InetAddress destinationIp = InetAddress.getByName(httpRequest.getUrl().getHost());
-        // UDP udpSocket = new UDP(addressIp, port);        => Useful ?
         DatagramSocket socket = new DatagramSocket();   // Will be given an ephemeral port
 
+        // Setup sending scheduler
+        PacketScheduler scheduler = new PacketScheduler(socket);
 
-        // TODO: Handshaking to peer + sync sequence number
-        handshakeInit(socket, sequenceNumberRegistry, destinationIp, destinationPort);
+        PseudoTCPMessage requestPacketMessage = new PseudoTCPMessage(
+                ByteArrayUtils.bytesToStringIP(destinationIp.getAddress()),
+                destinationPort,
+                httpRequest.toString().getBytes());
 
+        // TODO: (Handshaking) Establish reliable connection with peer
 
+        // TODO: Start a receiver thread of some sort to receive the ACK. (In Scheduler)
+
+        // Schedule the message's packets to be sent
+        scheduler.queuePackets(requestPacketMessage.getPackets(), sequenceNumberRegistry);
+
+        // TODO: Once message sent entirely, listen for response message
+
+        // TODO: Close connection
+
+        // TODO: Return reconstructed HTTPResponse( string )
         return null;
     }
 
@@ -34,83 +47,9 @@ public class PseudoTCP implements IProtocol {
         SelectiveRepeatRegistry sequenceNumberRegistry = new SelectiveRepeatRegistry();
         DatagramSocket socket = new DatagramSocket(port);
 
-        byte[] buffer = new byte[1024];
-        DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
-        socket.receive(receivedPacket);
-
         // TODO: Detect that it's a SYN packet type
 
-        handshakeReception(socket, receivedPacket, sequenceNumberRegistry);
-
     }
 
-    private void handshakeInit(DatagramSocket socket, SelectiveRepeatRegistry seqReg, InetAddress ip, int port) {
-        // PseudoTCPPacket packet = new PseudoTCPPacket();
-
-        // TODO: These Future Task should be generic for any packet
-        Runnable synTask = () -> {
-            seqReg.sync(13);    // For test
-
-            // TODO: Replace with Packet class
-            byte[] buffer = new byte[1024];
-            buffer[0] = (byte) seqReg.requestNext();    // Should be 13 for this test
-            DatagramPacket dtPacket = new DatagramPacket(buffer, buffer.length, ip, port);
-            try {
-                socket.send(dtPacket);
-            } catch (IOException e) {
-                // TODO
-                e.printStackTrace();
-            }
-
-            // Wait for SYN-ACK
-            DatagramPacket synackPacket = new DatagramPacket(buffer, buffer.length);
-            try {
-                socket.receive(synackPacket);
-            } catch (IOException e) {
-                // TODO
-                e.printStackTrace();
-            }
-
-            // For testing
-            System.out.println("Received SYN-ACK value: " + synackPacket.getData()[0]);
-
-            // TODO: If so, send ack to conclude handshaking init
-
-        };
-
-        Future synFuture = _pool.submit(synTask);
-
-        //
-        try {
-            synFuture.get(TIMEOUT_DELAY_MS, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException | ExecutionException e) {
-            // TODO
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            // TODO: HANDSHAKING FAILURE
-        }
-    }
-
-    private void handshakeReception(DatagramSocket socket, DatagramPacket receivedSynPacket, SelectiveRepeatRegistry seqReg) throws IOException {
-        // TODO: Clean + Replace with proper class
-
-        // TEST
-        byte receivedByte = receivedSynPacket.getData()[0];
-        System.out.println("Received SYN value: " + receivedByte);
-
-        seqReg.sync(receivedByte);
-
-        // Answer back after sync
-        InetAddress incomingAddress = receivedSynPacket.getAddress();
-        int incomingPort = receivedSynPacket.getPort();
-
-        byte[] buffer = new byte[1024];
-        buffer[0] = (byte) seqReg.requestNext();    // Should be the received seq number
-        receivedSynPacket = new DatagramPacket(buffer, buffer.length, incomingAddress, incomingPort);
-        socket.send(receivedSynPacket);
-
-        // TODO: Receive ACK for the SYN-ACK (In a Future)
-
-    }
 
 }
--- src/HttpLib/protocol/UDP/PseudoTCPTest.java
+++ src/HttpLib/protocol/UDP/PseudoTCPTest.java
@@ -13,32 +13,32 @@ class PseudoTCPTest {
 
     @Test
     void manualTesting() throws IOException, InterruptedException {
-        PseudoTCP protocol = new PseudoTCP();
-
-        HttpRequest req;
-        try {
-            req = new HttpRequest(new URL("http://localhost/"), HttpRequestMethod.GET, new HttpMessageHeader());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        IRequestCallback callback = request -> new HttpResponse();
-
-        Runnable listenTask = () -> {
-            try {
-                protocol.listen(9797, callback);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        };
-
-        Thread t = new Thread(listenTask);
-        t.start();
-
-        protocol.send(req, 9797);
-
-        t.join();
+//        PseudoTCP protocol = new PseudoTCP();
+//
+//        HttpRequest req;
+//        try {
+//            req = new HttpRequest(new URL("http://localhost/"), HttpRequestMethod.GET, new HttpMessageHeader());
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        IRequestCallback callback = request -> new HttpResponse();
+//
+//        Runnable listenTask = () -> {
+//            try {
+//                protocol.listen(9797, callback);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        };
+//
+//        Thread t = new Thread(listenTask);
+//        t.start();
+//
+//        protocol.send(req, 9797);
+//
+//        t.join();
     }
 
 }
